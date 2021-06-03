d0725aaca1e644dd929706abf144d87a840c5b04
James-Livesey <jamesliveg@gmail.com>
2021-06-03 18:10:51 +0100

    Add basic text rendering

--- canvas.js
+++ canvas.js
@@ -1,20 +1,45 @@
-export const VIRTUAL_WIDTH = 640;
-export const VIRTUAL_HEIGHT = 480;
+export const DISP_WIDTH = 640;
+export const DISP_HEIGHT = 480;
+export const DISP_SCALE_FACTOR = 2;
+export const TERM_COLS = 40;
+export const TERM_ROWS = 20;
+export const CHAR_WIDTH = DISP_WIDTH / TERM_COLS;
+export const CHAR_HEIGHT = DISP_HEIGHT / TERM_ROWS;
 
-export var element;
+const TEXT_FONT = `${CHAR_HEIGHT * DISP_SCALE_FACTOR}px "Brass Mono", monospace`;
 
-export function resize() {
+var element;
+var context;
+var readyListeners = [];
+
+export function onReady(callback) {
+    readyListeners.push(callback);
+}
+
+export function drawText(text, x, y) {
+    context.font = TEXT_FONT;
+    context.textBaseline = "middle";
+    context.textAlign = "center";
+
+    for (var i = 0; i < text.length; i++) {
+        context.fillText(text[i], x + ((i + 0.5) * CHAR_WIDTH * DISP_SCALE_FACTOR), CHAR_HEIGHT * DISP_SCALE_FACTOR / 2);
+    }
+}
+
+function resize() {
     var viewportWidth = window.innerWidth - 40;
     var viewportHeight = window.innerHeight - 40;
+    var scaledDisplayWidth = DISP_WIDTH * DISP_SCALE_FACTOR;
+    var scaledDisplayHeight = DISP_HEIGHT * DISP_SCALE_FACTOR;
 
     var width;
     var height;
 
-    if (VIRTUAL_HEIGHT * (viewportWidth / VIRTUAL_WIDTH) < viewportHeight) {
+    if (scaledDisplayHeight * (viewportWidth / scaledDisplayWidth) < viewportHeight) {
         width = viewportWidth;
-        height = VIRTUAL_HEIGHT * (viewportWidth / VIRTUAL_WIDTH);
+        height = scaledDisplayHeight * (viewportWidth / scaledDisplayWidth);
     } else {
-        width = VIRTUAL_WIDTH * (viewportHeight / VIRTUAL_HEIGHT);
+        width = scaledDisplayWidth * (viewportHeight / scaledDisplayHeight);
         height = viewportHeight;
     }
 
@@ -26,8 +51,14 @@ export function resize() {
 
 window.addEventListener("load", function() {
     element = document.querySelector("canvas");
+    context = element.getContext("2d");
+
+    element.width = DISP_WIDTH * DISP_SCALE_FACTOR;
+    element.height = DISP_HEIGHT * DISP_SCALE_FACTOR;
 
     resize();
+
+    readyListeners.forEach((i) => i());
 });
 
 window.addEventListener("resize", resize);
--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 import * as canvas from "./canvas.js";
+
+canvas.onReady(function() {
+    canvas.drawText("Hello, world!", 0, 0);
+});
