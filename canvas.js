817e1b177cd56b0359915253c85dfb22c3077f88
James-Livesey <jamesliveg@gmail.com>
2021-06-03 19:41:11 +0100

    Add rect drawing and styling options

--- canvas.js
+++ canvas.js
@@ -12,8 +12,39 @@ var element;
 var context;
 var readyListeners = [];
 
+export class Colour {
+    constructor(red, green, blue, alpha = 1) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
+    }
+
+    toCss() {
+        return `rgba(${this.red}, ${this.green}, ${this.blue}, ${this.alpha})`;
+    }
+}
+
+function twoPointsToPointSize(x1, y1, x2, y2) {
+    return [
+        Math.min(x1, x2),
+        Math.min(y1, y2),
+        Math.abs(x2 - x1),
+        Math.abs(y2 - y1)
+    ];
+}
+
 export function onReady(callback) {
     readyListeners.push(callback);
+}
+
+export function setColour(colour) {
+    context.fillStyle = colour.toCss();
+    context.strokeStyle = colour.toCss();
+}
+
+export function setStrokeWidth(width) {
+    context.lineWidth = width * DISP_SCALE_FACTOR;
 }
 
 export function drawText(text, x, y) {
@@ -24,6 +55,15 @@ export function drawText(text, x, y) {
     for (var i = 0; i < text.length; i++) {
         context.fillText(text[i], (x + ((i + 0.5) * CHAR_WIDTH)) * DISP_SCALE_FACTOR, (y + (CHAR_HEIGHT / 2)) * DISP_SCALE_FACTOR);
     }
+}
+
+export function drawRect(x1, y1, x2, y2) {
+    context.rect(...twoPointsToPointSize(x1 * DISP_SCALE_FACTOR, y1 * DISP_SCALE_FACTOR, x2 * DISP_SCALE_FACTOR, y2 * DISP_SCALE_FACTOR));
+    context.stroke();
+}
+
+export function fillRect(x1, y1, x2, y2) {
+    context.fillRect(...twoPointsToPointSize(x1 * DISP_SCALE_FACTOR, y1 * DISP_SCALE_FACTOR, x2 * DISP_SCALE_FACTOR, y2 * DISP_SCALE_FACTOR));
 }
 
 function resize() {
@@ -57,6 +97,9 @@ window.addEventListener("load", function() {
     element.height = DISP_HEIGHT * DISP_SCALE_FACTOR;
 
     resize();
+
+    setColour(new Colour(0, 0, 0));
+    setStrokeWidth(1);
 
     readyListeners.forEach((i) => i());
 });
--- script.js
+++ script.js
@@ -2,4 +2,5 @@ import * as canvas from "./canvas.js";
 
 canvas.onReady(function() {
     canvas.drawText("Hello, world!", 0, 0);
+    canvas.drawRect(16, 24, 32, 48);
 });
