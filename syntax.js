cf09c6c7b63f1f35c53bcbe2dcab3030db78c838
James-Livesey <jamesliveg@gmail.com>
2021-06-07 19:27:49 +0100

    Add start of basic tokenisation and fix syntax highlighting order

--- syntax.js
+++ syntax.js
@@ -1,14 +1,16 @@
 import * as canvas from "./canvas.js";
 import * as term from "./term.js";
 
-const RE_KEYWORD = /(?<![a-z])(?:print|input|goto|if|then|else|end|for|to|step|next)(?![a-z])/i;
 const RE_STRING_LITERAL = /"(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*'|`(?:[^`\\]|\\.)*`/i;
+const RE_LINE_NUMBER = /^\d+/i;
+const RE_KEYWORD = /(?<![a-z])(?:print|input|goto|if|then|else|end|for|to|step|next)(?![a-z])/i;
 
 const RE_OR = /|/;
 
 const RE_ALL = new RegExp([
-    RE_KEYWORD.source,
-    RE_STRING_LITERAL.source
+    RE_STRING_LITERAL.source,
+    RE_LINE_NUMBER.source,
+    RE_KEYWORD.source
 ].join(RE_OR.source), "gi");
 
 const KEYWORD_COLOURS = {
@@ -39,6 +41,16 @@ function setColourByName(name) {
     return canvas.setColour(canvas.colourScheme[canvas.COLOUR_NAMES[name]]);
 }
 
+export class Token {
+    constructor(code, lineNumber = null) {
+        this.code = code;
+        this.lineNumber = lineNumber;
+    }
+}
+
+export class ExecutionLabel extends Token {}
+export class Keyword extends Token {}
+
 export function highlight(code, index, col, row) {
     var defaultBackground = term.backgroundColour;
     var defaultForeground = term.foregroundColour;
@@ -63,18 +75,20 @@ export function highlight(code, index, col, row) {
 
         term.setColours(new canvas.Colour(0, 0, 0, 0), defaultForeground);
 
-        if (RE_KEYWORD.exec(match)) {
-            var keyword = match.toString().toLocaleLowerCase();
-
-            if (index == start) {
-                setColourByName(KEYWORD_COLOURS[keyword].background);
-                renderBackgroundHighlight(length, col, row);
-            }
-
-            term.foreground(KEYWORD_COLOURS[keyword].foreground);
-        } else if (RE_STRING_LITERAL.exec(match)) {
+        if (RE_STRING_LITERAL.exec(match)) {
             clean();
             term.foreground("green");
+        } else if (RE_KEYWORD.exec(match)) {
+            var keyword = match.toString().toLocaleLowerCase();
+
+            if (Object.keys(KEYWORD_COLOURS).includes(keyword)) {
+                if (index == start) {
+                    setColourByName(KEYWORD_COLOURS[keyword].background);
+                    renderBackgroundHighlight(length, col, row);
+                }
+    
+                term.foreground(KEYWORD_COLOURS[keyword].foreground);
+            }
         }
 
         term.goto(col, row);
@@ -82,4 +96,38 @@ export function highlight(code, index, col, row) {
     }
 
     clean();
+}
+
+export function tokeniseLine(code, lineNumber = null) {
+    var tokens = [];
+    var lineSymbols = [];
+    var match;
+
+    while (match = RE_ALL.exec(code)) {
+        lineSymbols.push(match[0]);
+    }
+
+    for (var i = 0; i < lineSymbols.length; i++) {
+        if (RE_LINE_NUMBER.exec(lineSymbols[i]) && i == 0) {
+            tokens.push(new ExecutionLabel(lineSymbols[i], lineNumber));
+
+            continue;
+        }
+
+        if (RE_KEYWORD.exec(lineSymbols[i])) {
+            tokens.push(new Keyword(lineSymbols[i], lineNumber));
+        }
+    }
+}
+
+export function tokenise(program) {
+    var tokens = [];
+
+    for (var i = 0; i < program.length; i++) {
+        if (typeof(program[i]) != "string") {
+            continue;
+        }
+
+        tokens = tokens.concat(tokeniseLine(program[i], i));
+    }
 }
