00c8623c71165474a2601d64da21eb7fc0cfae3b
James-Livesey <jamesliveg@gmail.com>
2024-08-27 19:48:40 +0100

    Allow theme to be set via URL parameter

--- theme.js
+++ theme.js
@@ -1,9 +1,21 @@
+import * as core from "./core.js";
+
 function detectIsDarkMode() {
     return (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches);
 }
 
-const IS_DARK_MODE = detectIsDarkMode();
+const IS_DARK_MODE = core.getParameter("theme") != null ? (core.getParameter("theme") == "dark") : detectIsDarkMode();
 
 export function isDarkMode() {
     return IS_DARK_MODE;
 }
+
+window.addEventListener("load", function() {
+    if (core.getParameter("theme") == "light") {
+        document.body.classList.add("lightTheme");
+    }
+
+    if (core.getParameter("theme") == "dark") {
+        document.body.classList.add("darkTheme");
+    }
+});
